d0ac578f3d80a6be7af98fafcf317e0e9dc8c73e
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-04-02 10:02:57 +0700

    Initial Commit

---
+++ ScrollProgressTracker/ScrollProgressTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollProgressTrackerApp.swift
+//  ScrollProgressTracker
+//
+//  Created by Naufal Adli on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollProgressTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
